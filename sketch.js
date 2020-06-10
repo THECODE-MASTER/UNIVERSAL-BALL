88ad0de2a775b5a52c44ea29a311c6ae7ebf3d84
THECODE-MASTER <63726176+THECODE-MASTER@users.noreply.github.com>
2020-06-10 18:48:44 +0530

    UNIVERSAL BALL

--- sketch.js
+++ sketch.js
@@ -1,9 +1,12 @@
-var ball;
+var ball,database,position;
 
 function setup(){
+    database=firebase.database();
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    var ballposition=database.ref('ball/position');
+    ballposition.on("value",readposition,showerror);
 }
 
 function draw(){
@@ -24,6 +27,19 @@ function draw(){
 }
 
 function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+   database.ref('ball/position').set({
+       'x':position.x+x,
+       'y':position.y+y
+   })
+
+}
+
+function readposition(data){
+    position=data.val();
+    ball.x=position.x;
+    ball.y=position.y;
+}
+
+function showerror(){
+    console.log("error");
 }
